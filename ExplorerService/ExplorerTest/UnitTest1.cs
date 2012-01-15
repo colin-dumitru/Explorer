07d2daa1de26fce0b6f1a7b7052b7d8149149024
Iustina Melinte <iustina.melinte@gmail.com>
2012-01-15 18:09:09 +0200

    list drives, insert into sql DB

--- ExplorerService/ExplorerTest/UnitTest1.cs
+++ ExplorerService/ExplorerTest/UnitTest1.cs
@@ -3,12 +3,17 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DBLibrary;
+using System.Data.SqlClient;
 
 namespace ExplorerTest {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestMethod1() {
+        public void main() {
+            DBWriter.LogFile = @"C:\Users\bkt\Desktop\explorer\ExplorerService\dblog.txt";
+            DBWriter.Initialize();
+            DBWriter.ListDrive("C:");
         }
     }
 }
